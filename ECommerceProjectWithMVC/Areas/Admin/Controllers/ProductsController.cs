1efa4521fb0150223f2cf0113af77a604f742bef
Talish <talishesu@code.edu.az>
2022-03-02 22:46:42 +0400

    Product Finished

--- ECommerceProjectWithMVC/Areas/Admin/Controllers/ProductsController.cs
+++ ECommerceProjectWithMVC/Areas/Admin/Controllers/ProductsController.cs
@@ -36,6 +36,41 @@ namespace ECommerceProjectWithMVC.Areas.Admin.Controllers
         }
 
 
+
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            if (id < 1)
+            {
+                return NotFound();
+            }
+
+            
+            var vm = new ProductDetailViewModel();
+
+
+            vm.Product = await db.Products
+                .Include(p => p.Images.Where(i => i.DeletedTime == null))
+                .FirstOrDefaultAsync(p=>p.Id == id && p.DeletedTime == null);
+            if (vm.Product == null)
+            {
+                return NotFound();
+            }
+            vm.Product.Brand = await db.Brands.FirstOrDefaultAsync(b => b.Id == vm.Product.BrandId);
+            vm.Product.Category = await db.Categories.FirstOrDefaultAsync(b => b.Id == vm.Product.CategoryId);
+            vm.ProductImages = await db.ProductImages.Where(pi=>pi.DeletedTime == null &&pi.ProductId ==id).ToListAsync();
+            vm.SpecificationProductItems = await db.SpecificationProductItems.Where(spi=>spi.ProductId == id && spi.DeletedTime ==null).ToListAsync();
+            vm.Specifications = await db.Specifications.Where(s=>s.DeletedTime == null).ToListAsync();
+
+
+            vm.AllSizes = await db.Sizes.Where(s=>s.DeletedTime==null).ToListAsync();
+            vm.AllColors = await db.Colors.Where(s=>s.DeletedTime==null).ToListAsync();
+            vm.ProductPricings = await db.ProductPricings.Where(pp=>pp.ProductId == id && pp.DeletedTime ==null).ToListAsync();
+
+            return View(vm);
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Create()
         {
@@ -92,7 +127,7 @@ namespace ECommerceProjectWithMVC.Areas.Admin.Controllers
             var selectSpec = productFormModel.SelectedSpecifications.Where(ss => ss.Value != null);
 
 
-            var specListForCategory = db.SpecificationCategoryItems.Where(spi => spi.CategoryId == productFormModel.Product.CategoryId);
+            var specListForCategory =await db.SpecificationCategoryItems.Where(spi => spi.CategoryId == productFormModel.Product.CategoryId).ToListAsync();
 
 
 
@@ -303,189 +338,296 @@ namespace ECommerceProjectWithMVC.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit([FromRoute] int id, ProductFormModel productFormModel)
         {
-            if (id != productFormModel.Product.Id)
-            {
-                return BadRequest();
-            }
-
-
-            if (productFormModel.Product.Files == null && !productFormModel.Product.Files.Any())
-            {
-                ModelState.AddModelError("Product.Files", "Add Photo!");
-            }
-
-
-
-
-
-            var selectSpec = productFormModel.SelectedSpecifications.Where(ss => ss.Value != null);
-
-
-            var specListForCategory = db.SpecificationCategoryItems.Where(spi => spi.CategoryId == productFormModel.Product.CategoryId);
-
-
-
-            foreach (var item in selectSpec)
-            {
-                var t = specListForCategory.Where(slc => slc.SpecificationId == item.Id);
-                if (!t.Any())
+                if (id != productFormModel.Product.Id)
                 {
-                    ModelState.AddModelError("Specifications", "An unspecified specification is written for the selected category!!");
+                    return BadRequest();
                 }
-            }
 
 
-            if (productFormModel.ProductPricings == null)
-            {
-                ModelState.AddModelError("Colors", "Please Create New Pricing!!");
-            }
-            else
-            {
-                foreach (var item in productFormModel.ProductPricings)
+                if (productFormModel.Product.Files == null)
+                {
+                    ModelState.AddModelError("Product.Files", "Add Photo!");
+                }
+
+
+
+
+
+                var selectSpec = productFormModel.SelectedSpecifications.Where(ss => ss.Value != null);
+
+
+                var specListForCategory = await db.SpecificationCategoryItems.Where(spi => spi.CategoryId == productFormModel.Product.CategoryId).ToListAsync();
+
+
+
+                foreach (var item in selectSpec)
+                {
+                    var t = specListForCategory.Where(slc => slc.SpecificationId == item.Id);
+                    if (!t.Any())
+                    {
+                        ModelState.AddModelError("Specifications", "An unspecified specification is written for the selected category!!");
+                    }
+                }
+
+
+                if (productFormModel.ProductPricings == null)
+                {
+                    var propri = await db.ProductPricings.Where(p => p.ProductId == id && p.DeletedTime == null).ToListAsync();
+                    if (propri == null)
+                    {
+                        ModelState.AddModelError("Colors", "Please Create New Pricing!!");
+                    }
+                }
+                else
                 {
 
-                    var oldpp = productFormModel.ProductPricings.Where(pp => pp.ColorId == item.ColorId && pp.SizeId == item.SizeId).ToList();
-                    if (oldpp.Count() > 1)
+
+                    foreach (var item in productFormModel.ProductPricings)
                     {
-                        ModelState.AddModelError("Colors", "Size and Color Cannot Be Similar!!");
-                    }
-                    else
-                    {
-                        if (item.Price == 0 || item.Price == null)
+
+                        var oldpp = productFormModel.ProductPricings.Where(pp => pp.ColorId == item.ColorId && pp.SizeId == item.SizeId).ToList();
+
+
+
+                        if (oldpp.Count() > 1)
                         {
-                            ModelState.AddModelError("Colors", "Fill in all the fields!!");
+                            ModelState.AddModelError("Colors", "Size and Color Cannot Be Similar!!");
+                        }
+                        else
+                        {
+                            if (item.Price == 0 || item.Price == null)
+                            {
+                                ModelState.AddModelError("Colors", "Fill in all the fields!!");
+                            }
+
+                            if (item.ColorId == 0 || item.ColorId == null)
+                            {
+                                ModelState.AddModelError("Colors", "Fill in all the fields!!");
+                            }
+                            if (item.SizeId == 0 || item.SizeId == null)
+                            {
+                                ModelState.AddModelError("Colors", "Fill in all the fields!!");
+                            }
                         }
 
-                        if (item.ColorId == 0 || item.ColorId == null)
-                        {
-                            ModelState.AddModelError("Colors", "Fill in all the fields!!");
-                        }
-                        if (item.SizeId == 0 || item.SizeId == null)
-                        {
-                            ModelState.AddModelError("Colors", "Fill in all the fields!!");
-                        }
                     }
-
                 }
-            }
 
 
 
 
 
-            if (!ModelState.IsValid)
-            {
-                var vm = new ProductCreateViewModel();
+                if (!ModelState.IsValid)
+                {
+                    var vm = new ProductCreateViewModel();
 
-                var brands = await db.Brands.Where(b => b.DeletedTime == null).ToListAsync();
-                var selectList = new SelectList(brands, "Id", "Name");
-                vm.Brands = selectList;
-
-
-                var categories = await db.Categories
-                    .Include(c => c.Children)
-                    .Where(b => b.DeletedTime == null).ToListAsync();
-                var selectList2 = new SelectList(categories, "Id", "Name");
-                vm.Categories = selectList2;
+                    var brands = await db.Brands.Where(b => b.DeletedTime == null).ToListAsync();
+                    var selectList = new SelectList(brands, "Id", "Name");
+                    vm.Brands = selectList;
 
 
-
-
-                vm.Specifications = await db.Specifications.Where(s => s.DeletedTime == null).ToListAsync();
-
-
-                var sizes = await db.Sizes.Where(b => b.DeletedTime == null).ToListAsync();
-                var selectList3 = new SelectList(sizes, "Id", "Name");
-                vm.Sizes = selectList3;
-
-
-
-                var colors = await db.Colors.Where(b => b.DeletedTime == null)
-                    .Select(c => new { Id = c.Id, Text = $"{c.Name}({c.ColorHexCode})" })
-                    .ToListAsync();
-                var selectList4 = new SelectList(colors, "Id", "Text");
-                vm.Colors = selectList4;
-
-                vm.AllSizes = await db.Sizes.Where(b => b.DeletedTime == null).ToListAsync();
-                vm.AllColors = await db.Colors.Where(b => b.DeletedTime == null).ToListAsync();
-
-
-                vm.Product = await db.Products.FirstOrDefaultAsync(pp => pp.Id == id);
-
-                vm.SpecificationProductItems = await db.SpecificationProductItems.Where(SPI => SPI.ProductId == id).ToListAsync();
-                vm.ProductImages = await db.ProductImages.Where(p => p.ProductId == id && p.DeletedTime == null).ToListAsync();
-                vm.ProductPricings = await db.ProductPricings.Where(pp => pp.ProductId == id).ToListAsync();
-
-
-
-                return View(vm);
-            }
+                    var categories = await db.Categories
+                        .Include(c => c.Children)
+                        .Where(b => b.DeletedTime == null).ToListAsync();
+                    var selectList2 = new SelectList(categories, "Id", "Name");
+                    vm.Categories = selectList2;
 
 
 
 
-            var entity = await db.Products
-                .Include(p => p.Images.Where(i => i.DeletedTime == null))
-                .Include(p => p.SpecificationItems.Where(i => i.DeletedTime == null))
-                .Include(p => p.PriceList.Where(i => i.DeletedTime == null))
-                .FirstOrDefaultAsync(p => p.Id == id && p.DeletedTime == null);
+                    vm.Specifications = await db.Specifications.Where(s => s.DeletedTime == null).ToListAsync();
 
-            if (entity == null)
+
+                    var sizes = await db.Sizes.Where(b => b.DeletedTime == null).ToListAsync();
+                    var selectList3 = new SelectList(sizes, "Id", "Name");
+                    vm.Sizes = selectList3;
+
+
+
+                    var colors = await db.Colors.Where(b => b.DeletedTime == null)
+                        .Select(c => new { Id = c.Id, Text = $"{c.Name}({c.ColorHexCode})" })
+                        .ToListAsync();
+                    var selectList4 = new SelectList(colors, "Id", "Text");
+                    vm.Colors = selectList4;
+
+                    vm.AllSizes = await db.Sizes.Where(b => b.DeletedTime == null).ToListAsync();
+                    vm.AllColors = await db.Colors.Where(b => b.DeletedTime == null).ToListAsync();
+
+
+                    vm.Product = await db.Products.FirstOrDefaultAsync(pp => pp.Id == id);
+
+                    vm.SpecificationProductItems = await db.SpecificationProductItems.Where(SPI => SPI.ProductId == id).ToListAsync();
+                    vm.ProductImages = await db.ProductImages.Where(p => p.ProductId == id && p.DeletedTime == null).ToListAsync();
+                    vm.ProductPricings = await db.ProductPricings.Where(pp => pp.ProductId == id).ToListAsync();
+
+
+
+                    return View(vm);
+                }
+
+
+                if (productFormModel.ProductPricings != null)
+                {
+                    foreach (var item in db.ProductPricings.Where(p => p.ProductId == id))
+                    {
+                        db.ProductPricings.Remove(item);
+                    }
+
+                    foreach (var item in productFormModel.ProductPricings)
+                    {
+
+
+                        ProductPricing newPP = new ProductPricing();
+                        newPP.ColorId = item.ColorId;
+                        newPP.Price = item.Price;
+                        newPP.ProductId = productFormModel.Product.Id;
+                        //newPP.Product = productFormModel.Product;
+                        newPP.SizeId = item.SizeId;
+
+                        newPP.CreatedByUserId = 1;
+                        await db.ProductPricings.AddAsync(newPP);
+                        //await db.SaveChangesAsync();
+                    }
+                }
+
+
+
+                foreach (var item in db.SpecificationProductItems.Where(p => p.ProductId == id))
+                {
+                     db.SpecificationProductItems.Remove(item);
+                }
+
+
+                foreach (var item in selectSpec)
+                {
+                    SpecificationProductItem newSCI = new SpecificationProductItem();
+                    newSCI.SpecificationId = item.Id;
+                    newSCI.ProductId = productFormModel.Product.Id;
+                    newSCI.Value = item.Value;
+                    //newSCI.Product = productFormModel.Product;
+                    newSCI.Specification = await db.Specifications.FirstOrDefaultAsync(s => s.Id == item.Id);
+                    newSCI.CreatedByUserId = 1;
+                    await db.SpecificationProductItems.AddAsync(newSCI);
+                    //await db.SaveChangesAsync();
+                }
+
+
+
+
+
+                var entity = await db.Products
+                    .Include(p => p.Images.Where(i => i.DeletedTime == null))
+                    .Include(p => p.SpecificationItems.Where(i => i.DeletedTime == null))
+                    .Include(p => p.PriceList.Where(i => i.DeletedTime == null))
+                    .FirstOrDefaultAsync(p => p.Id == id && p.DeletedTime == null);
+
+                if (entity == null)
+                {
+                    return NotFound();
+                }
+
+                entity.SKU = productFormModel.Product.SKU;
+                entity.Name = productFormModel.Product.Name;
+                entity.ShortDescription = productFormModel.Product.ShortDescription;
+                entity.BrandId = productFormModel.Product.BrandId;
+                entity.Description = productFormModel.Product.Description;
+
+
+                #region Compare Images
+                foreach (var image in productFormModel.Product.Files)
+                {
+
+                    if (image.Id != null && image.Id > 0)
+                    {
+                        var current = entity.Images.FirstOrDefault(i => i.Id == image.Id);
+                        if (current != null)
+                        {
+                            if (string.IsNullOrWhiteSpace(image.TempPath)) //viewde silinme kimi vurulub
+                            {
+
+                                current.DeletedByUserId = 1;
+                                current.DeletedTime = DateTime.Now;
+
+                            }
+                            else //viewde oldugu kimi gelib,yada ismain deyishib
+                            {
+                                current.IsMain = image.IsMain;
+                            }
+                        }
+                    }
+                    else if (image.File != null) //yeni fayllar
+                    {
+                        var extension = Path.GetExtension(image.File.FileName); //.jpg
+                        var fileName = $"product-{Guid.NewGuid()}{extension}".ToLower();
+                        var phsicalPath = Path.Combine(env.ContentRootPath, "wwwroot", "uploads", "products", "images", fileName);
+
+                        using (var fs = new FileStream(phsicalPath, FileMode.Create, FileAccess.Write))
+                        {
+                            await image.File.CopyToAsync(fs);
+                        }
+
+                        entity.Images.Add(new ProductImages
+                        {
+                            ImagePath = fileName,
+                            IsMain = image.IsMain
+                        });
+                    }
+                }
+                #endregion
+
+
+                await db.SaveChangesAsync();
+               
+            return RedirectToAction("Index");
+        }
+
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id < 1)
             {
                 return NotFound();
             }
 
-            entity.SKU = productFormModel.Product.SKU;
-            entity.Name = productFormModel.Product.Name;
-            entity.ShortDescription = productFormModel.Product.ShortDescription;
-            entity.BrandId = productFormModel.Product.BrandId;
-            entity.Description = productFormModel.Product.Description;
-
-
-            #region Compare Images
-            foreach (var image in productFormModel.Product.Files)
+            var product = await db.Products.FirstOrDefaultAsync(b => b.Id == id);
+            if (product == null)
             {
-
-                if (image.Id != null && image.Id > 0)
-                {
-                    var current = entity.Images.FirstOrDefault(i => i.Id == image.Id);
-                    if (current != null)
-                    {
-                        if (string.IsNullOrWhiteSpace(image.TempPath)) //viewde silinme kimi vurulub
-                        {
-
-                            current.DeletedByUserId = 1;
-                            current.DeletedTime = DateTime.Now;
-
-                        }
-                        else //viewde oldugu kimi gelib,yada ismain deyishib
-                        {
-                            current.IsMain = image.IsMain;
-                        }
-                    }
-                }
-                else if (image.File != null) //yeni fayllar
-                {
-                    var extension = Path.GetExtension(image.File.FileName); //.jpg
-                    var fileName = $"product-{Guid.NewGuid()}{extension}".ToLower();
-                    var phsicalPath = Path.Combine(env.ContentRootPath, "wwwroot", "uploads", "products", "images", fileName);
-
-                    using (var fs = new FileStream(phsicalPath, FileMode.Create, FileAccess.Write))
-                    {
-                        await image.File.CopyToAsync(fs);
-                    }
-
-                    entity.Images.Add(new ProductImages
-                    {
-                        ImagePath = fileName,
-                        IsMain = image.IsMain
-                    });
-                }
+                return NotFound();
             }
-            #endregion
+            if (product.DeletedTime != null)
+            {
+                return NotFound();
+            }
 
-
+            product.DeletedTime = DateTime.Now;
+            product.DeletedByUserId = 1;
             await db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Reverse(int id)
+        {
+            if (id < 1)
+            {
+                return NotFound();
+            }
+
+            var product = await db.Products.FirstOrDefaultAsync(b => b.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            if (product.DeletedTime == null)
+            {
+                return NotFound();
+            }
+
+            product.DeletedTime = null;
+            product.DeletedByUserId = null;
+            await db.SaveChangesAsync();
+
             return RedirectToAction("Index");
         }
     }
---
+++ ECommerceProjectWithMVC/Models/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceProjectWithMVC.Models.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace ECommerceProjectWithMVC.Models.ViewModels
+{
+    public class ProductDetailViewModel
+    {
+        public Product Product { get; set; }
+        public List<ProductImages> ProductImages { get; set; }
+        public List<Specification> Specifications { get; set; }
+        public List<SpecificationProductItem> SpecificationProductItems { get; set; }
+        public List<Color> AllColors { get; set; }
+        public List<Size> AllSizes { get; set; }
+        public List<ProductPricing> ProductPricings { get; set; }
+    }
+}
